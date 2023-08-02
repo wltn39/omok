20357b05cae23d55b64c34747cf699ffcc3b4521
gino <wltn39@gmail.com>
2023-08-02 18:10:10 +0900

    first commit

---
+++ lib/omok.dart
@@ -0,0 +1,11 @@
+class omok{
+  String? omokDate;
+  int? win; 
+  int? tie;
+  int? defeat;
+  int? downCount;
+  int? score;
+
+  Omok({this.omokDate, this.win, this.tie, this.defeat, this.downCount, this,score});
+  
+}
